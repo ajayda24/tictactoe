691c8c863c5eca41ee6b968168f94abe8ed059e2
Ajay Daniel Trevor <ajaydtrevor@gmail.com>
2021-09-17 23:56:25 +0530

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { useState, Fragment } from 'react'
-import { checkWinner, checkMainWinner } from '../helpers/checkMultipleIF'
+import { checkWinner, checkMainWinner,checkText } from '../helpers/checkMultipleIF'
 
 export default function Home() {
   const [currentIcon, setCurrentIcon] = useState(true)
@@ -8,7 +8,10 @@ export default function Home() {
 
   return (
     <div className='p-1 h-screen'>
-      <div id='gameScreen' className='min-h-full grid grid-cols-3 grid-rows-3 gap-1'>
+      <div
+        id='gameScreen'
+        className='min-h-full grid grid-cols-3 grid-rows-3 gap-1'
+      >
         {simpleArray.map((i) => (
           <div
             key={i}
@@ -29,21 +32,37 @@ export default function Home() {
                   const Xwinner = checkWinner('X', i)
                   const Ywinner = checkWinner('O', i)
                   const currentCellWinner = Xwinner || Ywinner
-                  var parent = e.target.parentElement.innerHTML
                   if (currentCellWinner) {
-                    document.getElementById(`${i}-0`).innerHTML = `${currentCellWinner}`
+                    document.getElementById(
+                      `${i}-0`
+                    ).innerHTML = `${currentCellWinner}`
+                  } else {
+                    const a = []
+                    for (let q = 1; q < 10; q++) {
+                      if(checkText(i,q) == 'X' || checkText(i,q) == 'O'){
+                        a.push(checkText(i,q))
+                      }
+                    }
+                    if(a.length == 9){
+                      console.log('draw')
+                      for (let w = 1; w < 10; w++) {
+                        document.getElementById(`${i}-${w}`).innerText = ''
+                      }
+                    }
                   }
 
                   if (checkMainWinner('X')) {
                     setMainWinner('X')
                     console.log('X wins')
                     alert('X Wins')
-                    document.getElementById('gameScreen').style.pointerEvents = 'none'
+                    document.getElementById('gameScreen').style.pointerEvents =
+                      'none'
                   } else if (checkMainWinner('O')) {
                     setMainWinner('O')
                     console.log('O wins')
                     alert('O Wins')
-                    document.getElementById('gameScreen').style.pointerEvents = 'none'
+                    document.getElementById('gameScreen').style.pointerEvents =
+                      'none'
                   }
                 }}
               ></div>
